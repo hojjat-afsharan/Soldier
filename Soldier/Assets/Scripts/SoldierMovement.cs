ddab69f66455077b8f60a6242bb1c59d03ea765f
Hojat Afsharan <afsharan.h@gmail.com>
2018-07-17 17:15:47 -0700

    Unity-Setup
    Pixel per uniset to 32

---
+++ Soldier/Assets/Scripts/SoldierMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SoldierMovement : MonoBehaviour {
+
+	private void Start () 
+	{
+		//transform.position = new Vector2(transform.position.x , transform.position.y +1);
+	}
+	
+	private void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			CheckTouchedPlace();
+		}
+	}
+
+	private static void CheckTouchedPlace()
+	{
+		var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+		if (hit != null && hit.collider != null)
+		{
+			Debug.Log("I'm hitting " + hit.collider.name);
+		}
+	}
+}
