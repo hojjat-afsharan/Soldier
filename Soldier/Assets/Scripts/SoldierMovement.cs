05dcb9ef2d7f4fb2bf27674c7fc7da8458f992be
Hojat Afsharan <afsharan.h@gmail.com>
2018-07-17 17:46:35 -0700

    Move Player To Movable Places
    Lerp is not completed

--- Soldier/Assets/Scripts/SoldierMovement.cs
+++ Soldier/Assets/Scripts/SoldierMovement.cs
@@ -2,9 +2,26 @@
 
 public class SoldierMovement : MonoBehaviour {
 
+	public Vector3 startMarker;
+	public Vector3 endMarker;
+
+	// Movement speed in units/sec.
+	public float speed = 0.01F;
+
+	// Time when the movement started.
+	private float startTime;
+
+	// Total distance between the markers.
+	private float journeyLength;
+	private bool movablePlaceTouched = false;
+	private RaycastHit2D hit;
+	
 	private void Start () 
 	{
-		//transform.position = new Vector2(transform.position.x , transform.position.y +1);
+		// Keep a note of the time the movement started.
+		startTime = Time.time;
+
+		
 	}
 	
 	private void Update () 
@@ -13,15 +30,44 @@ public class SoldierMovement : MonoBehaviour {
 		{
 			CheckTouchedPlace();
 		}
+
+		if (movablePlaceTouched)
+		{
+			MoveToMovablePlace();
+		}
 	}
 
-	private static void CheckTouchedPlace()
+	private void MoveToMovablePlace()
+	{
+		//TODO: player lerp to a position
+//		if (startMarker == endMarker)
+//			movablePlaceTouched = false;
+
+		startMarker = transform.position;
+		endMarker = hit.collider.transform.position;
+		// Calculate the journey length.
+		journeyLength = Vector3.Distance(startMarker, endMarker);
+
+		// Distance moved = time * speed.
+		float distCovered = (Time.time - startTime) * speed;
+
+		// Fraction of journey completed = current distance divided by total distance.
+		float fracJourney = distCovered / journeyLength;
+
+		// Set our position as a fraction of the distance between the markers.
+		transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+	}
+
+	private void CheckTouchedPlace()
 	{
 		var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+		hit = Physics2D.Raycast(pos, Vector2.zero);
 		if (hit != null && hit.collider != null)
 		{
-			Debug.Log("I'm hitting " + hit.collider.name);
+			if (hit.collider.CompareTag("MovablePlaces"))
+			{
+				movablePlaceTouched = true;
+			}
 		}
 	}
 }
